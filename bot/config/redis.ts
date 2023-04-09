f0922428d8c9921444ad056052eb55ab70336fdf
JapanPanda <japanxpanda@yahoo.com>
2023-04-08 17:31:47 -0700

    Fixed redis setup

--- bot/config/redis.ts
+++ bot/config/redis.ts
@@ -2,11 +2,11 @@ import { createClient } from "redis";
 import { Config } from "./config";
 import { Logger } from "./logger";
 
-export const Redis = createClient();
+export const Redis = createClient({ url: `redis://${Config.redisHost}:${Config.redisPort}` });
 
 export const startRedis = (): Promise<void> => {
     Logger.info("Initializing Redis");
-    return Redis.connect({ url: `redis://${Config.redisHost}:${Config.redisPort}` }).then(() => {
+    return Redis.connect().then(() => {
         Logger.info("Successfully initialized Redis");
     });
 };
